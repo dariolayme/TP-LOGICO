8275fad50472cd53b1f3b83f753c10af3b522740
Christian Huanca <chrishuancabarreto@gmail.com>
2019-07-05 02:07:41 -0300

    Agrego Candidatos

---
+++ TP-LOGICO.pl
@@ -0,0 +1,8 @@
+candidato(frank,partidoRojo,50).
+candidato(claire,partidoRojo,52).
+candidato(garret,partidoAzul,64).
+candidato(jackie,partidoAmarillo,38).
+candidato(linda,partidoAzul,30).
+candidato(catherine,partidoRojo,59).
+candidato(seth,partidoAmarillo,_).
+candidato(heather,partidoAmarillo,50).
